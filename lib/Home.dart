aa4c64e8dd457095f96d1188972d7081aa902a5c
bilaliqbal1 <muhammadbilaliqbal98@gmai.com>
2021-06-15 20:48:12 +0500

    creating widgets

---
+++ lib/Home.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // body: Column(
+      //   // mainAxisAlignment: MainAxisAlignment.center,
+      //   children: [
+      //     Center(
+      //       child: Align(
+      //         alignment: Alignment.topLeft,
+      //         child: Container(
+      //           margin: EdgeInsets.only(top: 20),
+      //           padding: EdgeInsets.only(left: 20, right: 20, top: 20),
+      //           child: Text("Box decoration"),
+      //           height: 200,
+      //           width: 200,
+      //           decoration: BoxDecoration(
+      //               // gradient: LinearGradient(colors: [
+      //               //   Color(0xffdde1),
+      //               // ]),
+      //               color: Colors.red[200],
+      //               borderRadius: BorderRadius.circular(20)),
+      //         ),
+      //       ),
+      //     ),
+      //   ],
+      // ),
+      // body: Center(
+      //   child: Text(
+      //     "Hello flutter",
+      //     style: TextStyle(
+      //         color: Colors.red,
+      //         fontFamily: "Helvetica",
+      //         fontSize: 20,
+      //         fontWeight: FontWeight.bold),
+      //   ),
+      // ),
+      // body: Center(
+      //   child: CircleAvatar(
+      //     // backgroundColor: Colors.yellow.withOpacity(1.0),
+      //     // backgroundImage: NetworkImage(
+      //     //     'https://images.unsplash.com/photo-1552519507-da3b142c6e3d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y2Fyc3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80'),
+      //     backgroundImage: AssetImage('assets/image.gfif'),
+      //     radius: 200,
+      //   ),
+      // ),
+      body: Stack(
+        children: [
+          Positioned(
+              left: 20,
+              top: 20,
+              child: Container(
+                color: Colors.red,
+                width: 100,
+                height: 100,
+              )),
+          Container(
+            color: Colors.red,
+            width: 100,
+            height: 100,
+          ),
+          Container(
+            color: Colors.red,
+            width: 100,
+            height: 100,
+          ),
+          Container(
+            color: Colors.red,
+            width: 100,
+            height: 100,
+          ),
+          Container(
+            color: Colors.red,
+            width: 100,
+            height: 100,
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,63 @@
+import 'package:firstapp/Home.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyApp());
 }
 
+// simple login code
+// class MyApp extends StatelessWidget {
+//   const MyApp({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     var materialApp = MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: Center(child: Text("Login Page")),
+//         ),
+//         body: Center(
+//           child: Column(
+//             // mainAxisAlignment: MainAxisAlignment.spaceAround,
+//             children: [
+//               SizedBox(
+//                 height: 20,
+//               ),
+//               Container(width: 100, child: TextField()),
+//               SizedBox(
+//                 height: 20,
+//               ),
+//               Container(width: 100, child: TextField()),
+//               SizedBox(
+//                 height: 20,
+//               ),
+//               ElevatedButton(onPressed: () {}, child: Text("Login"))
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//     return materialApp;
+//   }
+// }
+
+// class Home extends StatefulWidget {
+//   const Home({Key? key}) : super(key: key);
+
+//   @override
+//   _HomeState createState() => _HomeState();
+// }
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-  
+
   @override
   Widget build(BuildContext context) {
-    var materialApp = MaterialApp(
-      debugShowCheckedModeBanner: false,
+    return MaterialApp(
       home: Scaffold(
-        body: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            Text("1st row"),  
-            Text("1st row"),  
-            Text("1st row"),  
-        ],)
+        body: Home(),
       ),
     );
-    return materialApp;
   }
 }
